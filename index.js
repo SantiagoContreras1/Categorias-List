c9530fe64996245fefdf78d47da6981d9dbc8b1e
SantiagoContreras1 <168132510+SantiagoContreras1@users.noreply.github.com>
2024-08-30 20:34:21 -0600

    Initial commit frontend

---
+++ index.js
@@ -0,0 +1,6 @@
+const encabezadoUno = document.getElementById("encabezadoUno");
+encabezadoUno.innerHTML = "Hola mundo";
+
+const img = document.createElement("img");
+img.src = "";
+document.body.append(img);
